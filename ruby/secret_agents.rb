f9d78241fcae40fe0f09cd8a78cd8aebcc3aa84c
omarchaeopteryx <omaralimalik@gmail.com>
2016-09-17 16:41:04 -0700

    Finish challenge 4.6, through Release 5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -12,15 +12,17 @@ def encrypt(string)
 	while i < string.length
 		if string[i] == " "
 			string[i] = string[i]
+		elsif string[i] == "z"
+			string[i] = "a"
 		else
 			string[i] = string[i].next
 		end	
 		i += 1
 	end
-	string
+	puts string
 end
 
-encrypt("cat dog")
+# encrypt("cat dog")
 
 # 2. Decrypt pseudocode
 
@@ -47,8 +49,39 @@ def decrypt(string)
 	decrypted_string
 end
 
-decrypt("dbu eph")
+# puts decrypt("dbu eph")
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
 
+# puts decrypt(encrypt("swordfish"))
 
+# The code is solving for one method, and then using the results of that method as the input
+# into the next method, when they're nested (as in the "swordfish" example). This works because
+# both methods are returning a string, but if one method returns something else, it wouldn't work
+# because you can't decrypt something that's not a string.
 
+# release 5 - putting in our driver code!
 
+# Ask agent/user whether they want to decrypt or encrypt password
+
+puts "Hello. Would you like to decrypt or encrypt a password?"
+answer = gets.chomp
+
+# Ask for password
+
+puts "What is the word you would like to run?"
+keyword = gets.chomp
+
+# Conduct requested operation, print, exit
+
+if answer == "decrypt"
+	print decrypt(keyword)
+elsif answer == "encrypt"
+	print encrypt(keyword)
+else
+	puts "You must be a spy! Goodbye."
+end
+
+exit
