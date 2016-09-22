f3e086000830c294125c59f590ea8c2fc12f6bb9
omarchaeopteryx <omaralimalik@gmail.com>
2016-09-22 15:49:25 -0700

    Add pseudocode for 5.2

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+# Pseudocode
+
+# Build an empty hash structure.
+
+# Ask an interior designer to enter the details of a client.
+
+# Go through a list of questions, including string and numerical and boolean answers, and store the answers.
+
+# # Be sure to convert answers to appropriate types (string, integer, etc).
+
+# When done, PRINT the hash.
+
+# Ask user if they want to update anything in the list.
+
+## If yes, ask which KEY, then ask for the VALUE of that KEY and then change that KEY's value.
+
+## If 'none' skip to below.
+
+# PRINT the new hash.
