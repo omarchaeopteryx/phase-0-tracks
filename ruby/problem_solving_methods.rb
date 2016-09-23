bd924e2932b8b845a02fbd129fc492f78407be7d
omarchaeopteryx <omaralimalik@gmail.com>
2016-09-23 13:33:30 -0700

    Work on Release 2 for pair challenge

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -75,3 +75,44 @@ p fib(100)
 # confirm if it worked by doing fib(100) and comparing it to the number: 218922995834555169026
 
 p fib(100)[-1] == 218922995834555169026 # Testing for true/false
+
+
+# Release 2 - Sorting Methods
+
+# Given a set of random integers
+
+random = [1,4,6,78,945,733,44,30,7,89,234,33,1]
+
+# We want to sort it from lower number to highest number
+
+# Get the length of the list
+
+def sorter(array)
+  l = array.length
+  swapped = false
+  # Start a loop, do it number of times one less than the length of the array
+  p array
+  loop do
+    (l-1).times do |x|
+      p "Looping:"
+      ## Look at the first two numbers in the list. If the first item's value is greater than the second item's value, then swap the first item to the right.
+      if array[x] > array[x+1]
+        p "Testing:"
+        var1 = array[x] #shifted right, + 1
+        var2 = array[x+1] # shifted left, - 1
+        array[x] = var2
+        array[x+1] = var1
+        swapped = true # Turn swap to true, and start the loop over again
+      else
+        ## If not, do nothing
+      end
+      p array
+      # If there's no swap that goes on, swap = false and code will break
+    end
+    break if swapped == true
+  end
+    array
+end
+# End the loop
+
+sorter(random)
