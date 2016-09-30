2410a36d61451dbb47e1a47ebe4e7f48066c3959
omarchaeopteryx <omaralimalik@gmail.com>
2016-09-30 09:54:59 -0700

    Make Shout mixin module and test it for 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -5,16 +5,43 @@
 # Declaring a new module:
 
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+
+  def yell_angrily(words)
+      puts "#{words}" + "!!!" + " :("
   end
-  def self.yell_happily(words)
-    words + "!!!!" + " ^_^"
+
+  def shout_out_loud(phrase)
+    puts "Boldly declaring, #{(phrase)}!"
   end
 end
 
-# Adding some driver code:
+class Paperboy
+  include Shout
+end
 
-Shout.yell_angrily("how can you")
+class Auctioneer
+  include Shout
+end
 
-Shout.yell_happily("oh my goodness")
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(words)
+#     words + "!!!!" + " ^_^"
+#   end
+# end
+#
+# # Adding some driver code:
+#
+# Shout.yell_angrily("how can you")
+#
+# Shout.yell_happily("oh my goodness")
+
+paperboy = Paperboy.new
+paperboy.shout_out_loud("EXTRA, EXTRA! READ ALL ABOUT IT")
+paperboy.yell_angrily("TERRIBLE NEWS")
+
+auctioneer = Auctioneer.new
+auctioneer.shout_out_loud("AAAAAND SOLD! To the highest bidder")
+auctioneer.yell_angrily("No takers?")
