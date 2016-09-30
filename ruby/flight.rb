c07ccdf924b8eded7f72fa551f1bb2ef46dae752
omarchaeopteryx <omaralimalik@gmail.com>
2016-09-30 09:34:31 -0700

    Do flight.rb module

---
+++ ruby/flight.rb
@@ -0,0 +1,23 @@
+# OM, DBC 6.4
+
+# **Learning about modules (continued)**
+
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+
+class Bird
+  include Flight
+end
+
+class Plane
+  include Flight
+end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
