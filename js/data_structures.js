8e91c3864eac3b35cdaa0ab61b147465ba4f23dc
omarchaeopteryx <omaralimalik@gmail.com>
2016-10-05 12:41:35 -0700

    Make a few cars with Cars function

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,31 @@ for (var i = 0; i<colors.length; i++) {
 };
 
 console.log(horse);
+
+// Goal: create a few cars with a constructor function
+
+// It should have a few properties, data types, and one function
+
+function Cars(color,efficiency,model) {
+  console.log("About our car:",this);
+  this.color = color;
+  this.efficiency = efficiency;
+  this.model = model;
+  this.zoom = function() { console.log("VrrooooOoOOoooom!"); };
+  console.log("Car completed.");
+}
+
+// Making some cars
+var carA = new Cars("green",60,"city");
+var carB = new Cars("blue",15,"truck");
+var carC = new Cars("red",14,"sports");
+
+// Printing some cars to console
+console.log(carA);
+console.log(carB);
+console.log(carC);
+
+// Trying some functions
+carA.zoom();
+carB.zoom();
+carC.zoom();
